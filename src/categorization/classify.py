66e2fe18050457cae6893117fe2f59731a10332d
Lijo <lijoraju37@gmail.com>
2025-05-24 17:49:06 +0530

    feat(classification, scraping): improve label phrasing and fetch news by category

--- src/categorization/classify.py
+++ src/categorization/classify.py
@@ -4,7 +4,19 @@ import json
 
 classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli", device= -1)
 
-DEFAULT_LABELS = ["Technology", "Business", "Health", "Sports", "Science", "Politics"]
+DEFAULT_LABELS = ["Technology", "Business", "Health", "Sports", 
+                  "Science", "Politics", "Entertainment and Celebrity News", "Stock Market and Investments"]
+
+LABEL_MAP = {
+    "Technology": "Technology",
+    "Business": "Business",
+    "Politics": "Politics",
+    "Sports": "Sports",
+    "Health": "Health",
+    "Science": "Science",
+    "Entertainment and Celebrity News": "Entertainment",
+    "Stock Market and Investments": "Stock Market"
+}
 
 def classify_articles(input_path, output_path, candidate_labels=DEFAULT_LABELS, batch_size=8):
     with open(input_path, "r", encoding="utf-8") as f:
@@ -22,7 +34,7 @@ def classify_articles(input_path, output_path, candidate_labels=DEFAULT_LABELS, 
                 batch_results = [batch_results]
             for j, (article, result) in enumerate(zip(articles[i:i+batch_size], batch_results)):
                 tagged_article = article.copy()
-                tagged_article['category'] = result['labels'][0]
+                tagged_article['category'] = LABEL_MAP[result['labels'][0]]
                 classified.append(tagged_article)
         except Exception as e:
             print(f"⚠️ Error classifying batch {i}-{i+batch_size}:", e)
--- src/scraping/newsapi_scraper.py
+++ src/scraping/newsapi_scraper.py
@@ -8,47 +8,48 @@ from pathlib import Path
 load_dotenv()
 
 newsapi = NewsApiClient(api_key=os.getenv("NEWS_API_KEY"))
+categories = {"business", "entertainment", "general", "health", "science", "sports", "technology"}
 
 def save_articles(articles, output_path="data/raw/newsapi_articles.json"):
     Path("data/raw").mkdir(parents=True, exist_ok=True)
     with open(output_path, "w", encoding="utf-8") as f:
         json.dump(articles, f, indent=2, ensure_ascii=False)
 
-def fetch_news(query="trending news", from_days_ago=2, language='en', page_size=20, max_pages=3, save=False):
+def fetch_news(query="AI", from_days_ago=1, language='en', page_size=20, max_pages=3, save=False):
     from_date = (datetime.now() - timedelta(days=from_days_ago)).strftime('%Y-%m-%d')
     all_articles = []
+    for category in categories:
+        for page in range(1, max_pages + 1):
+            try:
+                # response = newsapi.get_everything(
+                #     from_param=from_date,
+                #     language=language,
+                #     sort_by='popularity',
+                #     page=page,
+                #     page_size=page_size
+                # )
+                response = newsapi.get_top_headlines(
+                    language=language,
+                    category=category,
+                    page_size=page_size,
+                    page=page
+                )
 
-    for page in range(1, max_pages + 1):
-        try:
-            response = newsapi.get_everything(
-                q=query,
-                from_param=from_date,
-                language=language,
-                sort_by='relevancy',
-                page=page,
-                page_size=page_size
-            )
-            # response = newsapi.get_top_headlines(
-            #     language=language,
-            #     page_size=page_size,
-            #     page=page
-            # )
+                articles = response.get('articles', [])
+                if not articles:
+                    break
 
-            articles = response.get('articles', [])
-            if not articles:
-                break
-
-            for article in articles:
-                all_articles.append({
-                    'title': article['title'],
-                    'description': article['description'],
-                    'url': article['url'],
-                    'published_at': article['publishedAt'],
-                    'source': article['source']['name'],
-                    'content':article['content']
-                })
-        except Exception as e:
-            print(f"Error on page {page}: {e}")
+                for article in articles:
+                    all_articles.append({
+                        'title': article['title'],
+                        'description': article['description'],
+                        'url': article['url'],
+                        'published_at': article['publishedAt'],
+                        'source': article['source']['name'],
+                        'content':article['content']
+                    })
+            except Exception as e:
+                print(f"Error on page {page}: {e}")
     
     if save:
         Path("data/raw").mkdir(parents=True, exist_ok=True)
